ecddae2ae87f78eebeb433148b34559f61e62c13
Marius CA <mariuskcodeacademy@gmail.com>
2023-04-12 15:37:42 +0300

    test redux store

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,8 +4,10 @@ import Alert from './../components/ui/Alert';
 import Feedback from '../components/ui/Feedback';
 import Grid from '../components/ui/grid/Grid';
 import Spinner from '../components/ui/Spinner';
+import { useSelector } from 'react-redux';
 
 function HomePage() {
+  const emailRedux = useSelector((state) => state.email);
   return (
     <Container>
       <Grid>
@@ -13,8 +15,9 @@ function HomePage() {
         <Alert type={'danger'}>Ivyko klaida</Alert>
         <Alert type={'success'}>success</Alert>
       </Grid>
+
       <Feedback show={true} type={'success'}>
-        Sekme
+        {emailRedux}
       </Feedback>
       <h1>HomePage</h1>
       <Spinner />
