a2d085148cc34b1edc8ca206bf15d7723b7cda97
Marius CA <mariuskcodeacademy@gmail.com>
2023-04-12 15:37:24 +0300

    provide store

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import AuthProvider from './store/AuthProvider';
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <BrowserRouter>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <Provider store={store}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </Provider>
   </BrowserRouter>,
   // </React.StrictMode>
 );
