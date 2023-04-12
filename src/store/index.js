ab8b8166f5a803fad04e77ed9fc4c2b8270f97e9
Marius CA <mariuskcodeacademy@gmail.com>
2023-04-12 15:30:01 +0300

    authSlice + store

---
+++ src/store/index.js
@@ -0,0 +1,31 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+const initAuthState = {
+  email: null,
+  token: null,
+  isLoggedIn: false,
+};
+
+const authSlice = createSlice({
+  name: 'authentication',
+  initialState: initAuthState,
+  reducers: {
+    login(state, action) {
+      state.token = action.payload.token;
+      state.email = action.payload.email;
+      state.isLoggedIn = true;
+    },
+    logout(state) {
+      state.token = null;
+      state.email = null;
+      state.isLoggedIn = false;
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: authSlice.reducer,
+});
+
+export const authActions = authSlice.actions;
+export default store;
